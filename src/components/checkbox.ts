259e80aa6a7b1f976e6b031daf4a70072c7cfcc8
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2025-06-24 11:08:04 +0200

    Bump package version to 0.0.39, update CheckboxComponent constructor to include id_factory and selected parameters, and enhance event handling methods

--- src/components/checkbox.ts
+++ src/components/checkbox.ts
@@ -1,30 +1,72 @@
 import { Component } from "../app";
 
+/**
+ * The `CheckboxComponent` class represents a checkbox component in the application.
+ * It manages the payload and element of the checkbox component and its selection state.
+ *
+ * @template P The type of the payload.
+ */
 export class CheckboxComponent<P> extends Component<P, HTMLDivElement> {
 
-  checkbox: HTMLInputElement;
+  private readonly checkbox: HTMLInputElement;
 
-  constructor(payload: P, label?: string) {
+  /**
+   * Creates a new CheckboxComponent.
+   * 
+   * @param payload The payload associated with the checkbox.
+   * @param id_factory A function to generate the ID for the checkbox based on the payload.
+   * @param selected Whether the checkbox is initially selected.
+   * @param label Optional label for the checkbox.
+   */
+  constructor(payload: P, id_factory: (payload: P) => string, selected: boolean = false, label?: string) {
     super(payload, document.createElement('div'));
     this.element.classList.add('form-check');
+
     this.checkbox = document.createElement('input');
     this.checkbox.type = 'checkbox';
     this.checkbox.classList.add('form-check-input');
+    this.checkbox.checked = selected;
+    this.checkbox.id = id_factory(payload);
     this.element.appendChild(this.checkbox);
+
     if (label) {
       const labelElement = document.createElement('label');
       labelElement.classList.add('form-check-label');
       labelElement.textContent = label;
+      labelElement.setAttribute('for', this.checkbox.id);
       this.element.appendChild(labelElement);
     }
+
     this.element.addEventListener('click', () => {
       if (this.checkbox.checked)
-        this.select();
+        this.selected();
       else
-        this.unselect();
+        this.unselected();
     });
   }
 
-  select(): void { }
-  unselect(): void { }
+  /**
+   * Sets the checked state of the checkbox.
+   * 
+   * @param checked The new checked state.
+   */
+  set_checked(checked: boolean): void {
+    this.checkbox.checked = checked;
+    if (checked)
+      this.selected();
+    else
+      this.unselected();
+  }
+
+  /**
+   * Callback when the checkbox is selected.
+   * Override this method to define custom behavior when the checkbox is selected.
+   */
+  selected(): void { }
+
+  /**
+   * Callback when the checkbox is unselected.
+   * Override this method to define custom behavior when the checkbox is unselected.
+   */
+  unselected(): void { }
 }
