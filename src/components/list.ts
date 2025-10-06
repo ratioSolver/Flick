18299a28bfeb889410095e92828e2017d8413add
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2025-10-06 13:23:27 +0200

    Bump package version to 0.1.13, and add CheckboxListItemComponent for enhanced list item functionality.

--- src/components/list.ts
+++ src/components/list.ts
@@ -108,3 +108,57 @@ export class ListItemComponent<P> extends PayloadComponent<HTMLLIElement, P> imp
   select(): void { this.a.classList.add('active'); }
   unselect(): void { this.a.classList.remove('active'); }
 }
+
+export class CheckboxListItemComponent<P> extends PayloadComponent<HTMLLIElement, P> implements Selector {
+
+  private readonly input: HTMLInputElement;
+  private readonly label: HTMLLabelElement;
+  private icn: Element;
+  private text: Text;
+
+  /**
+   * Creates an instance of CheckboxListItemComponent.
+   * 
+   * @param payload The payload associated with this element.
+   * @param id The unique identifier for the checkbox input element.
+   * @param icn The icon element to be displayed in the list item.
+   * @param text The text content of the list item.
+   */
+  constructor(payload: P, id: string, icn: Element, text: string) {
+    super(document.createElement('li'), payload);
+    this.node.classList.add('list-group-item', 'd-flex', 'align-items-center');
+
+    this.input = document.createElement('input');
+    this.input.type = 'checkbox';
+    this.input.id = id;
+    this.input.classList.add('form-check-input', 'me-2');
+    this.input.addEventListener('change', () => {
+      if (this.input.checked)
+        this.select();
+      else
+        this.unselect();
+    });
+    this.node.append(this.input);
+
+    this.label = document.createElement('label');
+    this.label.classList.add('form-check-label', 'd-flex', 'align-items-center', 'flex-grow-1', 'mb-0');
+    this.label.htmlFor = id;
+    this.icn = icn;
+    this.icn.classList.add('me-2');
+    this.label.append(this.icn);
+    this.text = document.createTextNode(text);
+    this.label.append(this.text);
+    this.node.append(this.label);
+  }
+
+  set_icon(icn: Element): void {
+    this.icn.replaceWith(icn);
+    this.icn = icn;
+    this.icn.classList.add('me-2');
+  }
+
+  set_text(text: string): void { this.text.textContent = text; }
+
+  select(): void { }
+  unselect(): void { }
+}
