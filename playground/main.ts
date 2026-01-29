2c5196022250eeaf542026e86b7aa1d2f8da412e
Riccardo De Benedictis <riccardo.debenedictis@cnr.it>
2026-01-29 14:24:51 +0100

    Refactor: Enhance App component with NavbarList and NavbarItem for improved navigation structure

--- playground/main.ts
+++ playground/main.ts
@@ -1,5 +1,9 @@
-import { App, Brand } from '../src/components/app';
+import { App, Brand, NavbarItem, NavbarList } from '../src/components/app';
 import { flick } from '../src/index';
 
-const rerender = flick('app', App(Brand('MyApp')));
+const rerender = flick('app', App(Brand('Flick'), NavbarList([
+    NavbarItem('Home', '#home', true),
+    NavbarItem('About', '#about'),
+    NavbarItem('Contact', '#contact')
+])));
 rerender();
--- src/components/app.ts
+++ src/components/app.ts
@@ -1,11 +1,11 @@
 import { h, VNode } from "snabbdom";
 
-export function Brand(name: string): VNode {
-  return h('a.navbar-brand', { props: { href: '#' } }, name);
+export function Brand(name: string, href: string = '#'): VNode {
+  return h('a.navbar-brand', { props: { href } }, name);
 }
 
-export function IconBrand(icon: string, width: number = 30, height: number = 30, name?: string): VNode {
-  return h('a.navbar-brand', { props: { href: '#' } }, [
+export function IconBrand(icon: string, width: number = 30, height: number = 30, name?: string, href: string = '#'): VNode {
+  return h('a.navbar-brand', { props: { href } }, [
     h('img', {
       props: {
         src: icon,
@@ -18,7 +18,20 @@ export function IconBrand(icon: string, width: number = 30, height: number = 30,
   ]);
 }
 
-function Navbar(brand: VNode, content: VNode): VNode {
+export function NavbarItem(text: string, href: string = '#', active: boolean = false): VNode {
+  return h('li.nav-item', [
+    h('a.nav-link', {
+      class: { active },
+      props: { href }
+    }, text)
+  ]);
+}
+
+export function NavbarList(items: VNode[]): VNode {
+  return h('ul.navbar-nav.me-auto.mb-2.mb-lg-0', items);
+}
+
+function Navbar(brand: VNode, navbar_content: VNode, id = 'navbarSupportedContent'): VNode {
   return h('nav.navbar.navbar-expand-lg.bg-body-tertiary', [
     h('div.container-fluid', [
       brand,
@@ -26,21 +39,21 @@ function Navbar(brand: VNode, content: VNode): VNode {
         props: {
           type: 'button',
           'data-bs-toggle': 'collapse',
-          'data-bs-target': '#navbarSupportedContent',
-          'aria-controls': 'navbarSupportedContent',
+          'data-bs-target': `#${id}`,
+          'aria-controls': id,
           'aria-expanded': 'false',
           'aria-label': 'Toggle navigation'
         }
       }, [
         h('span.navbar-toggler-icon')
       ]),
-      h('div#navbarSupportedContent.collapse.navbar-collapse', [content])
+      h(`div#${id}.collapse.navbar-collapse`, [navbar_content])
     ])
   ]);
 }
 
-export function App(brand: VNode = Brand('Flick'), content: VNode = h('!')): VNode {
+export function App(brand: VNode = Brand('Flick'), navbar_content: VNode = h('!')): VNode {
   return h('div', [
-    Navbar(brand, content),
+    Navbar(brand, navbar_content),
   ]);
 }
